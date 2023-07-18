662f5e2f082f91e2c0858719106158c3bf1a043e
manh12a5 <manh12a5@gmail.com>
2023-07-18 15:59:33 +0700

    Remove Value

--- src/main/java/com/example/demo/s3/S3Config.java
+++ src/main/java/com/example/demo/s3/S3Config.java
@@ -5,7 +5,7 @@ import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -14,11 +14,11 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${aws.s3.access-key}")
-    private String accessKey;
+    private final S3Credential s3Credential;
 
-    @Value("${aws.s3.secret-access-key}")
-    private String secretKey;
+    public S3Config(S3Credential s3Credential) {
+        this.s3Credential = s3Credential;
+    }
 
     @Bean
     public S3Client s3Client() {
@@ -30,7 +30,7 @@ public class S3Config {
     @Bean
     public AmazonS3 amazonS3() {
         BasicSessionCredentials basicSessionCredentials
-                = new BasicSessionCredentials(accessKey, secretKey, "");
+                = new BasicSessionCredentials(s3Credential.getAccessKey(), s3Credential.getSecretKey(), "");
 
         return AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.AP_SOUTHEAST_1)
